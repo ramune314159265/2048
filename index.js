396750de78d1bca20112b4f1d30dfa9c60ab793d
ramune314159265 <ramune314159265@gmail.com>
2024-07-10 16:58:35 +0900

    描画実装

--- index.js
+++ index.js
@@ -76,13 +76,16 @@ const run = () => {
 		}
 	}
 
-	class GameOutput extends EventRegister{
+	class GameOutput extends EventRegister {
+		static toDisplayNumber(state) {
+			return 2 ** state
+		}
 		constructor(game) {
 			super()
 			this.game = game
 			this.element = document.createElement('div')
-			this.game.field.data.forEach(line=>{
-				line.forEach(()=>{
+			this.game.field.data.forEach(line => {
+				line.forEach(() => {
 					const tile = document.createElement('div')
 					tile.classList.add('fieldTileBackground')
 					this.element.append(tile)
@@ -92,6 +95,64 @@ const run = () => {
 			this.element.style.gridTemplateColumns = `repeat(${fieldWidth}, 80px)`
 			this.element.style.gridTemplateRows = `repeat(${fieldHeight}, 80px)`
 			document.body.append(this.element)
+		}
+		addTile(x, y, state) {
+			const newElement = document.createElement('div')
+			newElement.textContent = GameOutput.toDisplayNumber(state)
+			newElement.classList.add('fieldTile')
+			newElement.style.translate = `${x * (80 + 16)}px ${y * (80 + 16)}px`
+			newElement.dataset.state = state
+			newElement.dataset.x = x
+			newElement.dataset.y = y
+			this.element.append(newElement)
+			newElement.animate([
+				{ scale: 0 },
+				{ scale: 1 }
+			], {
+				easing: 'cubic-bezier(0.22, 0.61, 0.36, 1)',
+				duration: 200
+			})
+		}
+		moveTile(x, y, toX, toY) {
+			const element = this.element.querySelector(`[data-x="${x}"][data-y="${y}"]`)
+			if (!element) {
+				return
+			}
+			element.dataset.x = toX
+			element.dataset.y = toY
+			element.style.translate = `${toX * (80 + 16)}px ${toY * (80 + 16)}px`
+		}
+		updateTile(x, y, state) {
+			const element = this.element.querySelector(`[data-x="${x}"][data-y="${y}"]`)
+			if (!element) {
+				return
+			}
+			element.textContent = GameOutput.toDisplayNumber(state)
+			element.dataset.state = state
+			element.animate([
+				{ scale: 1 },
+				{ scale: 1.1 },
+				{ scale: 1 }
+			], {
+				easing: 'cubic-bezier(0.22, 0.61, 0.36, 1)',
+				duration: 200
+			})
+		}
+		async removeTile(x,y){
+			const element = this.element.querySelector(`[data-x="${x}"][data-y="${y}"]`)
+			if (!element) {
+				return
+			}
+			element.dataset.x = ''
+			element.dataset.y = ''
+			await element.animate([
+				{ scale: 1 },
+				{ scale: 0 }
+			], {
+				easing: 'cubic-bezier(0.22, 0.61, 0.36, 1)',
+				duration: 200
+			}).finished
+			element.remove()
 		}
 	}
 
