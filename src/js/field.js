638387458b8e8d85b51586ad58ac1c6c4341eb5b
ramune314159265 <ramune314159265@gmail.com>
2024-08-19 13:32:10 +0900

    tilemoveをfieldに移動

--- src/js/field.js
+++ src/js/field.js
@@ -1,4 +1,4 @@
-import { outputCommands } from './enum.js'
+import { directions, outputCommands } from './enum.js'
 
 export class Field {
 	static emptyState = 0
@@ -49,5 +49,72 @@ export class Field {
 
 		this.game.io.output.emit(outputCommands.update,targetX, targetY, this.getTileState(targetX, targetY))
 		this.game.io.output.emit(outputCommands.remove,x, y, targetX, targetY)
+	}
+	move(direction) {
+		const moved = []
+		const directionDict = {
+			[directions.up]: 0,    //00
+			[directions.down]: 1,  //01
+			[directions.left]: 2,  //10
+			[directions.right]: 3, //11
+		}
+		const directionBit = directionDict[direction].toString(2).padStart(2, '0')
+		const xyConverterByDirection = (x, y) => {
+			let result = [x, y]
+			if (directionBit.at(-1) === '1') {
+				result = [this.game.config.fieldWidth - x - 1, this.game.config.fieldHeight - y - 1]
+			}
+			if (directionBit.at(-2) === '1') {
+				result = result.toReversed()
+			}
+			return result
+		}
+		this.data.forEach((line, y) => {
+			line.forEach((_, x) => {
+				const [convertedX, convertedY] = xyConverterByDirection(x, y)
+				if (!this.isTileFilled(convertedX, convertedY)) {
+					return
+				}
+				for (let i = 1; ; i++) {
+					// 上下方向
+					if (directionBit.at(-2) === '0') {
+						const positionY = directionBit.at(-1) === '1' ? convertedY + i : convertedY - i
+						if (!this.isTileFilled(convertedX, positionY)) {
+							continue
+						}
+						if (convertedY === positionY) {
+							continue
+						}
+						const selfTileState = this.getTileState(convertedX, convertedY)
+						if (selfTileState === this.getTileState(convertedX, positionY)) {
+							this.mergeTile(convertedX, convertedY, convertedX, positionY)
+							moved.push([convertedX, convertedY, convertedX, positionY])
+						} else {
+							this.moveTile(convertedX, convertedY, convertedX, directionBit.at(-1) === '1' ? positionY - 1 : positionY + 1)
+							moved.push([convertedX, convertedY, convertedX, directionBit.at(-1) === '1' ? positionY - 1 : positionY + 1])
+						}
+						break
+					} /*左右方向*/else if (directionBit.at(-2) === '1') {
+						const positionX = directionBit.at(-1) === '1' ? convertedX + i : convertedX - i
+						if (!this.isTileFilled(positionX, convertedY)) {
+							continue
+						}
+						if (convertedX === positionX) {
+							continue
+						}
+						const selfTileState = this.getTileState(convertedX, convertedY)
+						if (selfTileState === this.getTileState(positionX, convertedY)) {
+							this.mergeTile(convertedX, convertedY, positionX, convertedY)
+							moved.push([convertedX, convertedY, positionX, convertedY])
+						} else {
+							this.moveTile(convertedX, convertedY, directionBit.at(-1) === '1' ? positionX - 1 : positionX + 1, convertedY)
+							moved.push([convertedX, convertedY, directionBit.at(-1) === '1' ? positionX - 1 : positionX + 1, convertedY])
+						}
+						break
+					}
+				}
+			})
+		})
+		return moved.filter(i => i[0] !== i[2] || i[1] !== i[3])
 	}
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import { Config } from './configs.js'
-import { directions, gameControls, gameEvents } from './enum.js'
+import { gameControls, gameEvents } from './enum.js'
 import { Field } from './field.js'
 import html2canvas from './libraries/html2canvas.js'
 import { Record } from './record.js'
@@ -17,7 +17,7 @@ export class Game extends EventRegister {
 		this.io = new IOClass(this)
 		this.init()
 		this.io.input.onAny(direction => {
-			const moved = this.move(direction)
+			const moved = this.field.move(direction)
 			if (moved.length !== 0) {
 				this.appearTile()
 			}
@@ -50,73 +50,6 @@ export class Game extends EventRegister {
 		targetTiles.forEach(tile => {
 			this.field.addTile(tile[0], tile[1], randomFromArray(this.config.availableTiles))
 		})
-	}
-	move(direction) {
-		const moved = []
-		const directionDict = {
-			[directions.up]: 0,    //00
-			[directions.down]: 1,  //01
-			[directions.left]: 2,  //10
-			[directions.right]: 3, //11
-		}
-		const directionBit = directionDict[direction].toString(2).padStart(2, '0')
-		const xyConverterByDirection = (x, y) => {
-			let result = [x, y]
-			if (directionBit.at(-1) === '1') {
-				result = [this.config.fieldWidth - x - 1, this.config.fieldHeight - y - 1]
-			}
-			if (directionBit.at(-2) === '1') {
-				result = result.toReversed()
-			}
-			return result
-		}
-		this.field.data.forEach((line, y) => {
-			line.forEach((_, x) => {
-				const [convertedX, convertedY] = xyConverterByDirection(x, y)
-				if (!this.field.isTileFilled(convertedX, convertedY)) {
-					return
-				}
-				for (let i = 1; ; i++) {
-					// 上下方向
-					if (directionBit.at(-2) === '0') {
-						const positionY = directionBit.at(-1) === '1' ? convertedY + i : convertedY - i
-						if (!this.field.isTileFilled(convertedX, positionY)) {
-							continue
-						}
-						if (convertedY === positionY) {
-							continue
-						}
-						const selfTileState = this.field.getTileState(convertedX, convertedY)
-						if (selfTileState === this.field.getTileState(convertedX, positionY)) {
-							this.field.mergeTile(convertedX, convertedY, convertedX, positionY)
-							moved.push([convertedX, convertedY, convertedX, positionY])
-						} else {
-							this.field.moveTile(convertedX, convertedY, convertedX, directionBit.at(-1) === '1' ? positionY - 1 : positionY + 1)
-							moved.push([convertedX, convertedY, convertedX, directionBit.at(-1) === '1' ? positionY - 1 : positionY + 1])
-						}
-						break
-					} /*左右方向*/else if (directionBit.at(-2) === '1') {
-						const positionX = directionBit.at(-1) === '1' ? convertedX + i : convertedX - i
-						if (!this.field.isTileFilled(positionX, convertedY)) {
-							continue
-						}
-						if (convertedX === positionX) {
-							continue
-						}
-						const selfTileState = this.field.getTileState(convertedX, convertedY)
-						if (selfTileState === this.field.getTileState(positionX, convertedY)) {
-							this.field.mergeTile(convertedX, convertedY, positionX, convertedY)
-							moved.push([convertedX, convertedY, positionX, convertedY])
-						} else {
-							this.field.moveTile(convertedX, convertedY, directionBit.at(-1) === '1' ? positionX - 1 : positionX + 1, convertedY)
-							moved.push([convertedX, convertedY, directionBit.at(-1) === '1' ? positionX - 1 : positionX + 1, convertedY])
-						}
-						break
-					}
-				}
-			})
-		})
-		return moved.filter(i => i[0] !== i[2] || i[1] !== i[3])
 	}
 	isGameOver() {
 		const isAllTileFilled = this.field.data.flat().every(tile => tile !== Field.emptyState)
