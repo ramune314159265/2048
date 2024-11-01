242c4c6dd02d0a5069907df27c44909ad84f25fa
ramune314159265 <ramune314159265@gmail.com>
2024-11-01 14:30:51 +0900

    ロードボタン追加

--- src/js/io/inputs/index.js
+++ src/js/io/inputs/index.js
@@ -7,6 +7,14 @@ export class GameInput {
 
 		io.mainElement.querySelector('.reset').addEventListener('click', () => io.emit(inputCommands.restart))
 		io.mainElement.querySelector('.screenshot').addEventListener('click', () => io.game.screenshot())
+		io.mainElement.querySelector('.importExport').addEventListener('click', () => {
+			const saveData = JSON.stringify(io.game.session.getExportedData())
+			const inputtedData = prompt('セーブデータを出力しました\nセーブデータを入力して読み込み', saveData)
+			if (inputtedData === saveData || !inputtedData) {
+				return
+			}
+			io.game.loadExportedData(inputtedData)
+		})
 		io.mainElement.querySelector('.next').addEventListener('click', () => io.emit(inputCommands.next))
 		io.mainElement.querySelector('.previous').addEventListener('click', () => io.emit(inputCommands.previous))
 		io.mainElement.querySelector('.stepBar').addEventListener('change', e => io.emit(inputCommands.setStep, Number(e.target.value)))
