77f5ab79176597b3e6fd22e9687d2dee9966ad2c
ramune314159265 <ramune314159265@gmail.com>
2025-02-15 01:43:34 +0900

    modelにプレイさせる用

--- src/js/io/tfPlay.js
+++ src/js/io/tfPlay.js
@@ -17,17 +17,24 @@ export class TfPlayIO extends GameIO {
 		document.body.append(s)
 	}
 	async init() {
-		this.model = await tf.loadLayersModel('indexeddb://model-2048')
+		this.model = await tf.loadLayersModel('/model.json')
 		this.model.compile({
 			optimizer: tf.train.adam(),
 			loss: 'meanSquaredError'
 		})
-		console.log('loaded model from indexeddb')
+		console.log('loaded model')
 		this.play()
 	}
 	async play() {
 		const getState = () => {
 			return this.game.session.field.data.flat()
+		}
+		const getReward = (current, previous, isDone) => {
+			if (isDone) {
+				return -1
+			}
+			const reward = Math.log2(current - previous) - 0.05
+			return Number.isFinite(reward) ? reward : 0
 		}
 		const selectAction = (state) => {
 			const stateTensor = tf.tensor([state])
@@ -35,14 +42,32 @@ export class TfPlayIO extends GameIO {
 			const action = qValues.argMax(-1).dataSync()[0]
 			return action
 		}
+		const getMaxQValue = (nextState) => {
+			const nextStateTensor = tf.tensor([nextState])
+			const qValues = this.model.predict(nextStateTensor).dataSync()
+			const maxQValue = Math.max(...qValues)
+			return maxQValue
+		}
+		const gamma = 0.99
 		this.emit(inputCommands.restart)
+		let score = this.game.session.score
+		let step = this.game.session.step
+		let done = false
 
-		while (true) {
-			const action = selectAction(getState())
+		while (!done) {
+			const state = getState()
+			const action = selectAction(state)
 			this.emit(Object.values(directions)[action])
-			if (this.game.session.isGameOvered) {
-				break
-			}
+			done = this.game.session.isGameOvered
+			const nextState = getState()
+
+			const target = (this.game.session.step - step === 0 ? -0.5 : getReward(this.game.session.score, score, done)) + gamma * getMaxQValue(nextState)
+			const currentQValues = this.model.predict(tf.tensor([state])).dataSync()
+			currentQValues[action] = target
+
+			await this.model.fit(tf.tensor([state]), tf.tensor([currentQValues]))
+			score = this.game.session.score
+			step = this.game.session.step
 		}
 	}
 }
